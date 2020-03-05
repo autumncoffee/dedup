4bd51160fa3ccca822c4b2a31ed25ddf56bab65b
Gennadiy Filatov <kain.winterheart@gmail.com>
2020-03-05 14:12:40 +0300

    ignore pagecache

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include <ac-library/containers/rbtree/persistent.hpp>
 #include <ac-common/str.hpp>
 #include <string.h>
+#include <ac-common/utils/string.hpp>
 
 #ifdef RELEASE_FILESYSTEM
 #include <filesystem>
@@ -34,7 +35,7 @@ struct TItem {
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " [-d /path/to/hashdb] [-n] [-c] /path/to/dir [/path/to/dir2 ...]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [-d /path/to/hashdb] [-n] [-c] [-s 4096] /path/to/dir [/path/to/dir2 ...]" << std::endl;
         return 1;
     }
 
@@ -42,6 +43,7 @@ int main(int argc, char** argv) {
     std::string dbPath;
     bool dryRun(false);
     bool compareContent(false);
+    size_t chunkSize(4096);
 
     for (size_t i = 1; i < argc; ++i) {
         if (strcmp(argv[i], "-d") == 0) {
@@ -53,6 +55,10 @@ int main(int argc, char** argv) {
 
         } else if (strcmp(argv[i], "-c") == 0) {
             compareContent = true;
+
+        } else if (strcmp(argv[i], "-s") == 0) {
+            ++i;
+            NAC::NStringUtils::FromString(strlen(argv[i]), argv[i], chunkSize);
 
         } else {
             pathes.push_back(argv[i]);
@@ -87,7 +93,7 @@ int main(int argc, char** argv) {
                 continue;
             }
 
-            NAC::TFile file(nodeStr);
+            NAC::TFile file(nodeStr, NAC::TFile::ACCESS_RDONLY_DIRECT);
 
             if (!file) {
                 continue;
@@ -111,7 +117,29 @@ int main(int argc, char** argv) {
             }
 
             if (hashedStr.empty()) {
-                SHA256((const unsigned char*)file.Data(), file.Size(), hashed);
+                SHA256_CTX ctx;
+
+                if (SHA256_Init(&ctx) == 0) {
+                    continue;
+                }
+
+                auto chunks = file.Chunks(chunkSize);
+                bool ok(true);
+
+                while (auto chunk = chunks.Next()) {
+                    if (SHA256_Update(&ctx, (const unsigned char*)chunk.Data(), chunk.Size()) == 0) {
+                        ok = false;
+                        break;
+                    }
+                }
+
+                if (!ok) {
+                    continue;
+                }
+
+                if (SHA256_Final(hashed, &ctx) == 0) {
+                    continue;
+                }
 
                 static const char charTable[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
@@ -194,8 +222,8 @@ int main(int argc, char** argv) {
                 }
 
                 if (compareContent) {
-                    NAC::TFile masterFile(master);
-                    NAC::TFile currentFile(spec.Path);
+                    NAC::TFile masterFile(master, NAC::TFile::ACCESS_RDONLY_DIRECT);
+                    NAC::TFile currentFile(spec.Path, NAC::TFile::ACCESS_RDONLY_DIRECT);
 
                     if (!masterFile || !currentFile) {
                         continue;
@@ -205,7 +233,26 @@ int main(int argc, char** argv) {
                         continue;
                     }
 
-                    if (memcmp(masterFile.Data(), currentFile.Data(), masterFile.Size()) != 0) {
+                    auto masterChunks = masterFile.Chunks(chunkSize);
+                    auto currentChunks = currentFile.Chunks(chunkSize);
+                    bool ok(true);
+
+                    while (ok) {
+                        auto masterChunk = masterChunks.Next();
+                        auto currentChunk = currentChunks.Next();
+
+                        if (!masterChunk && !currentChunk) {
+                            break;
+
+                        } else if (masterChunk.Size() != currentChunk.Size()) {
+                            ok = false;
+
+                        } else {
+                            ok = memcmp(masterChunk.Data(), currentChunk.Data(), masterChunk.Size()) == 0;
+                        }
+                    }
+
+                    if (!ok) {
                         continue;
                     }
                 }
